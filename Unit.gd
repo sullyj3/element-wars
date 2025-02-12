0e69c29604dbef5fe9fc78d8c4c27515e81bf9b9
James Sully <sullyj3@gmail.com>
2025-02-12 18:17:33 +1100

    basic selection and multi-selection

--- Unit.gd
+++ Unit.gd
@@ -5,6 +5,9 @@ enum Element {fire, water, grass}
 
 const unit_scene = preload("res://unit.tscn")
 
+signal selection_toggled
+signal selected_individually
+
 static func create(element: Element, faction_id: int) -> Unit:
 	var unit = unit_scene.instantiate()
 	unit.element = element
@@ -35,9 +38,16 @@ func _process(delta: float) -> void:
 func _on_detector_body_entered(body: Node2D) -> void:
 	if body is Unit and body.faction_id != faction_id:
 		print(self, ": enemy detected.")
-	
+
+## Current just toggles the shader highlight
+func set_selected(selected: bool):
+	$Sprite2D.material.set_shader_parameter("selected", selected)
 
 func _on_click_target_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		selected = not selected
-		$Sprite2D.material.set_shader_parameter("selected", selected)
+	if event is InputEventMouseButton and \
+		event.pressed and \
+		event.button_index == MOUSE_BUTTON_LEFT:
+		if Input.is_physical_key_pressed(KEY_CTRL):
+			emit_signal("selection_toggled")
+		else:
+			emit_signal("selected_individually")
--- main.gd
+++ main.gd
@@ -1,5 +1,29 @@
 extends Node2D
 
+var selected_set = {}
+
+func handle_unit_selection_toggled(unit):
+	var selected = null
+	if selected_set.erase(unit):
+		selected = false
+		print(unit, " has been deselected")
+	else:
+		selected_set[unit] = null
+		selected = true
+		print(unit, " has been selected")
+	unit.set_selected(selected)
+
+func deselect_all():
+	for unit in selected_set:
+		unit.set_selected(false)
+	selected_set = {}
+
+func handle_unit_selected_individually(unit):
+	for previously_selected_unit in selected_set:
+		previously_selected_unit.set_selected(false)
+	selected_set = {unit: null}
+	unit.set_selected(true)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var win_size: Vector2 = get_viewport().get_visible_rect().size
@@ -10,13 +34,10 @@ func _ready() -> void:
 	
 	var i = 0
 	for element in elements:
-		var unit_deselected = Unit.create(element, 0)
-		unit_deselected.position = initial_position + Vector2(i * 100, 0)
-		add_child(unit_deselected)
-		
-		var unit_selected = Unit.create(element, 0)
-		unit_selected.selected = true
-		unit_selected.position = initial_position + Vector2(i * 100, 100)
-		add_child(unit_selected)
+		var unit = Unit.create(element, 0)
+		unit.position = initial_position + Vector2(i * 100, 0)
+		unit.selection_toggled.connect(handle_unit_selection_toggled.bind(unit))
+		unit.selected_individually.connect(handle_unit_selected_individually.bind(unit))
+		add_child(unit)
 		
 		i += 1
